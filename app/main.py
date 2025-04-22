89d45522e13a820505d0e97a98915d3f4826adeb
xsenia <kseniaenta@mail.ru>
2025-04-22 22:04:18 +0300

    Бот откливается на команду старт, высылает видео после след. сообщения

--- app/main.py
+++ app/main.py
@@ -9,6 +9,9 @@ from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart
 from aiogram.types import Message
+from aiogram.methods import send_video
+
+from bot_logic import cat
 
 load_dotenv()
 TOKEN = getenv('BOT_TOKEN')
@@ -16,24 +19,26 @@ TOKEN = getenv('BOT_TOKEN')
 # All handlers should be attached to the Router (or Dispatcher)
 
 dp = Dispatcher()
+bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
 
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    await message.answer(f"Hello, {html.bold(message.from_user.full_name)}!")
+    await message.answer(f"{html.bold(message.from_user.full_name)}, спасибо за подписку! Как у тебя сейчас дела с английским?")
 
 
 @dp.message()
 async def echo_handler(message: Message) -> None:
     try:
-        await message.send_copy(chat_id=message.chat.id)
+        await message.answer(f"Слышу!")
+        await bot.send_video(chat_id=message.chat.id, video=cat)
     except TypeError:
         await message.answer("Nice try!")
 
 
 async def main() -> None:
     # Initialize Bot instance with default bot properties which will be passed to all API calls
-    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
 
     # And the run events dispatching
     await dp.start_polling(bot)
